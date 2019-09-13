27d473b391b01290093866b3170311b358e18b90
bdandurand <bdandurand@anl.gov>
2019-09-13 11:30:33 -0500

    Slight addition to the prox param update of Zowe and Schramm.

--- src/PBM-DelfinoOliveira.jl
+++ src/PBM-DelfinoOliveira.jl
@@ -38,11 +38,18 @@ function PBM_DelfinoOliveira(opfdata,params,K,HEUR,node_data)
       node_data.ncuts = ntrlcuts
 	# UPDATING RHO AS NECESSARY TO CORRESPOND TO EXACT PENALTY
         # COMPUTING AGGREGATION INFORMATION
-      if ctr.eta < params.tol1 && node_data.agg_sg_norm < params.tol2 && node_data.epshat < params.tol3 
+      if mpsoln.eta < params.tol1 && node_data.agg_sg_norm < params.tol2 && node_data.epshat < params.tol3 
+        ctr=mpsoln
 	println("Convergence to within tolerance: ")
 	@printf("iter: %d\t(objval,eta)=(%.4f,%.2e)\t(t,rho)=(%.3f,%.3f)\t(err,||s||,epshat)=(%.2e,%.2e,%.2e)\n",
 	  kk,ctr.linobjval,ctr.eta,node_data.tVal,node_data.rho,node_data.linerr,node_data.agg_sg_norm,node_data.epshat)
 	break
+      elseif mpsoln.eta < params.tol1 && abs(tHigh-tLow) > 1e-2
+        tHigh=node_data.tVal
+	node_data.tVal=2/(1/tLow+1/tHigh)
+	@printf("iter: %d\t(objval,eta)=(%.4f,%.2e)\t(t,rho)=(%.3f,%.3f)\t(err,||s||,epshat)=(%.2e,%.2e,%.2e)\n",
+	  kk,mpsoln.linobjval,mpsoln.eta,node_data.tVal,node_data.rho,node_data.linerr,node_data.agg_sg_norm,node_data.epshat)
+	continue
       end
      # STEP 3
       node_data.sscval = ((mpsoln.linobjval - node_data.rhoUB*mpsoln.eta)-(ctr.linobjval - node_data.rhoUB*ctr.eta))/(mpsoln.linobjval-(ctr.linobjval - node_data.rhoUB*ctr.eta)) 
