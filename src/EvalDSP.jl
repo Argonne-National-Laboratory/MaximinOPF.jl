d30d74deadfea02243ddbeb289528e177e1aa27d
bdandurand <bdandurand@anl.gov>
2019-11-13 16:24:48 -0600

    Adding new post function based on PowerModels.jl library template to src/EvalDSP.jl, not finished.

--- src/EvalDSP.jl
+++ src/EvalDSP.jl
@@ -1,5 +1,60 @@
 include("utils.jl")
 
+function post_pf_feas(pm::AbstractPowerModel)
+    variable_voltage(pm, bounded = false)
+    variable_generation(pm, bounded = false)
+    variable_branch_flow(pm, bounded = false)
+    variable_dcline_flow(pm, bounded = false)
+
+    constraint_model_voltage(pm)
+    
+    # ADD SLACK VARS
+    #@variable(pm.model,
+    #@objective(pm.model, )
+
+    # ADD OBJECTIVE FUNCTION THAT SUMS ALL SLACKS
+
+    for (i,bus) in ref(pm, :ref_buses)
+        @assert bus["bus_type"] == 3
+        constraint_theta_ref(pm, i)
+        constraint_voltage_magnitude_setpoint(pm, i)
+    end
+
+    for (i,bus) in ref(pm, :bus)
+        constraint_power_balance(pm, i) # REPLACE
+
+        # PV Bus Constraints
+        if length(ref(pm, :bus_gens, i)) > 0 && !(i in ids(pm,:ref_buses))
+            # this assumes inactive generators are filtered out of bus_gens
+            @assert bus["bus_type"] == 2
+
+            constraint_voltage_magnitude_setpoint(pm, i)
+            for j in ref(pm, :bus_gens, i)
+                constraint_active_gen_setpoint(pm, j)
+            end
+        end
+    end
+
+    for i in ids(pm, :branch)
+        constraint_ohms_yt_from(pm, i) # REPLACE
+        constraint_ohms_yt_to(pm, i) # REPLACE
+    end
+
+    for (i,dcline) in ref(pm, :dcline)
+        #constraint_dcline(pm, i) not needed, active power flow fully defined by dc line setpoints
+        constraint_active_dcline_setpoint(pm, i)
+
+        f_bus = ref(pm, :bus)[dcline["f_bus"]]
+        if f_bus["bus_type"] == 1
+            constraint_voltage_magnitude_setpoint(pm, f_bus["index"])
+        end
+
+        t_bus = ref(pm, :bus)[dcline["t_bus"]]
+        if t_bus["bus_type"] == 1
+            constraint_voltage_magnitude_setpoint(pm, t_bus["index"])
+        end
+end
+
 function solveFullModelAC(opfdata,x_val)
 # The full lower level model, AC formulation
   # OBTAIN SHORTHAND PROBLEM INFORMATION FROM opfdata
