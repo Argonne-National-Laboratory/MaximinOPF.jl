ad52f08d91ea60914f0db79c639d80881ebbfffb
Kibaek Kim <kimk@anl.gov>
2020-04-23 12:43:34 -0500

    updated test scripts and packages

--- test/testcases.jl
+++ test/testcases.jl
@@ -1,171 +1,51 @@
 using PowerModels
 
-
 testcases = [
 	Dict(
 	"file" => "../data/case9.m", 
-	"PMOption" => SOCWRConicPowerModel,
- 	"name" => "case9SOCWR1",  	
+ 	"name" => "case9-1",
  	"attack_budget" => 3,
  	"inactive_indices" => [],
  	"protected_indices" => []
 	),
 	Dict(
 	"file" => "../data/case9.m", 
-	"PMOption" => SOCWRConicPowerModel,
- 	"name" => "case9SOCWR2",  	
+	"name" => "case9-2",
  	"attack_budget" => 3,
  	"inactive_indices" => [1,2,3,4,5,6,7,8,9],
  	"protected_indices" => []
 	),
 	Dict(
 	"file" => "../data/case9.m", 
-	"PMOption" => SOCWRConicPowerModel,
- 	"name" => "case9SOCWR3",  	
+	"name" => "case9-3",
  	"attack_budget" => 3,
  	"inactive_indices" => [],
  	"protected_indices" => [1,2,3,4,5,6,7,8,9]
 	),
 	Dict(
 	"file" => "../data/case9.m", 
-	"PMOption" => SOCWRConicPowerModel,
- 	"name" => "case9SOCWR4",  	
+	"name" => "case9-4",
  	"attack_budget" => 3,
  	"inactive_indices" => [1,4],
  	"protected_indices" => []
 	),
 	Dict(
 	"file" => "../data/case9.m", 
-	"PMOption" => SOCWRConicPowerModel,
- 	"name" => "case9SOCWR5",  	
+	"name" => "case9-5",
  	"attack_budget" => 3,
  	"inactive_indices" => [],
  	"protected_indices" => [2,3]
 	),
 	Dict(
 	"file" => "../data/case9.m", 
-	"PMOption" => SOCWRConicPowerModel,
- 	"name" => "case9SOCWR6",  	
+	"name" => "case9-6",
  	"attack_budget" => 3,
  	"inactive_indices" => [1,4,7],
  	"protected_indices" => [2,3,5,6,8,9]
 	),
 	Dict(
 	"file" => "../data/case9.m", 
-	"PMOption" => SOCWRConicPowerModel,
- 	"name" => "case9SOCWR7",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [1],
- 	"protected_indices" => [2]
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SDPWRMPowerModel,
- 	"name" => "case9SDPWR1",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [],
- 	"protected_indices" => []
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SDPWRMPowerModel,
- 	"name" => "case9SDPWR2",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [1,2,3,4,5,6,7,8,9],
- 	"protected_indices" => []
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SDPWRMPowerModel,
- 	"name" => "case9SDPWR3",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [],
- 	"protected_indices" => [1,2,3,4,5,6,7,8,9]
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SDPWRMPowerModel,
- 	"name" => "case9SDPWR4",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [1,4],
- 	"protected_indices" => []
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SDPWRMPowerModel,
- 	"name" => "case9SDPWR5",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [],
- 	"protected_indices" => [2,3]
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SDPWRMPowerModel,
- 	"name" => "case9SDPWR6",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [1,4,7],
- 	"protected_indices" => [2,3,5,6,8,9]
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SDPWRMPowerModel,
- 	"name" => "case9SDPWR7",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [1],
- 	"protected_indices" => [2]
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SparseSDPWRMPowerModel,
- 	"name" => "case9SpareseSDPWR1",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [],
- 	"protected_indices" => []
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SparseSDPWRMPowerModel,
- 	"name" => "case9SpareseSDPWR2",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [1,2,3,4,5,6,7,8,9],
- 	"protected_indices" => []
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SparseSDPWRMPowerModel,
- 	"name" => "case9SpareseSDPWR3",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [],
- 	"protected_indices" => [1,2,3,4,5,6,7,8,9]
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SparseSDPWRMPowerModel,
- 	"name" => "case9SpareseSDPWR4",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [1,4],
- 	"protected_indices" => []
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SparseSDPWRMPowerModel,
- 	"name" => "case9SpareseSDPWR5",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [],
- 	"protected_indices" => [2,3]
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SparseSDPWRMPowerModel,
- 	"name" => "case9SpareseSDPWR6",  	
- 	"attack_budget" => 3,
- 	"inactive_indices" => [1,4,7],
- 	"protected_indices" => [2,3,5,6,8,9]
-	),
-	Dict(
-	"file" => "../data/case9.m", 
-	"PMOption" => SparseSDPWRMPowerModel,
- 	"name" => "case9SpareseSDPWR7",  	
+	"name" => "case9-7",
  	"attack_budget" => 3,
  	"inactive_indices" => [1],
  	"protected_indices" => [2]
