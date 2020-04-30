15fc634c1df21fafa0219b2c959c206530e7d256
bdandurand <bdandurand@anl.gov>
2020-04-30 11:34:32 -0500

    Temporary fix to issue with resetting power flow bounds for linear models.

--- src/Variables.jl
+++ src/Variables.jl
@@ -32,49 +32,65 @@ function variable_branch_flow_slacks(pm::AbstractPowerModel; nw::Int=pm.cnw)
         if haskey(var(pm,nw),:p)
             p_fr = var(pm, nw,:p, f_idx)
             p_to = var(pm, nw,:p, t_idx)
-            if JuMP.has_upper_bound(p_fr)
-                @assert JuMP.upper_bound(p_fr) >= 0
-                JuMP.set_upper_bound(pt_br[f_idx,1], JuMP.upper_bound(p_fr))
-                JuMP.delete_upper_bound(p_fr)
+            if typeof(p_fr)==VariableRef
+                if JuMP.has_upper_bound(p_fr)
+                    @assert JuMP.upper_bound(p_fr) >= 0
+                    JuMP.set_upper_bound(pt_br[f_idx,1], JuMP.upper_bound(p_fr))
+                    JuMP.delete_upper_bound(p_fr)
+                end
+                if JuMP.has_lower_bound(p_fr)
+                    @assert JuMP.lower_bound(p_fr) <= 0
+                    JuMP.set_upper_bound(pt_br[f_idx,0], abs(JuMP.lower_bound(p_fr)))
+                    JuMP.delete_lower_bound(p_fr)
+                end
+            else
+                #TODO
             end
-            if JuMP.has_lower_bound(p_fr)
-                @assert JuMP.lower_bound(p_fr) <= 0
-                JuMP.set_upper_bound(pt_br[f_idx,0], abs(JuMP.lower_bound(p_fr)))
-                JuMP.delete_lower_bound(p_fr)
-            end
-            if JuMP.has_upper_bound(p_to)
-                @assert JuMP.upper_bound(p_to) >= 0
-                JuMP.set_upper_bound(pt_br[t_idx,1], JuMP.upper_bound(p_to))
-                JuMP.delete_upper_bound(p_to)
-            end
-            if JuMP.has_lower_bound(p_to)
-                @assert JuMP.lower_bound(p_to) <= 0
-                JuMP.set_upper_bound(pt_br[t_idx,0], abs(JuMP.lower_bound(p_to)))
-                JuMP.delete_lower_bound(p_to)
+            if typeof(p_to)==VariableRef
+                if JuMP.has_upper_bound(p_to)
+                    @assert JuMP.upper_bound(p_to) >= 0
+                    JuMP.set_upper_bound(pt_br[t_idx,1], JuMP.upper_bound(p_to))
+                    JuMP.delete_upper_bound(p_to)
+                end
+                if JuMP.has_lower_bound(p_to)
+                    @assert JuMP.lower_bound(p_to) <= 0
+                    JuMP.set_upper_bound(pt_br[t_idx,0], abs(JuMP.lower_bound(p_to)))
+                    JuMP.delete_lower_bound(p_to)
+                end
+            else
+                #TODO
             end
         end
         if haskey(var(pm,nw),:q)
             q_fr = var(pm, nw, :q, f_idx)
             q_to = var(pm, nw, :q, t_idx)
-            if JuMP.has_upper_bound(q_fr)
-                @assert JuMP.upper_bound(q_fr) >= 0
-                JuMP.set_upper_bound(qt_br[f_idx,1], JuMP.upper_bound(q_fr))
-                JuMP.delete_upper_bound(q_fr)
+            if typeof(q_fr)==VariableRef
+                if JuMP.has_upper_bound(q_fr)
+                    @assert JuMP.upper_bound(q_fr) >= 0
+                    JuMP.set_upper_bound(qt_br[f_idx,1], JuMP.upper_bound(q_fr))
+                    JuMP.delete_upper_bound(q_fr)
+                end
+                if JuMP.has_lower_bound(q_fr)
+                    @assert JuMP.lower_bound(q_fr) <= 0
+                    JuMP.set_upper_bound(qt_br[f_idx,0], abs(JuMP.lower_bound(q_fr)))
+                    JuMP.delete_lower_bound(q_fr)
+                end
+            else
+                #TODO
             end
-            if JuMP.has_lower_bound(q_fr)
-                @assert JuMP.lower_bound(q_fr) <= 0
-                JuMP.set_upper_bound(qt_br[f_idx,0], abs(JuMP.lower_bound(q_fr)))
-                JuMP.delete_lower_bound(q_fr)
-            end
-            if JuMP.has_upper_bound(q_to)
-                @assert JuMP.upper_bound(q_to) >= 0
-                JuMP.set_upper_bound(qt_br[t_idx,1], JuMP.upper_bound(q_to))
-                JuMP.delete_upper_bound(q_to)
-            end
-            if JuMP.has_lower_bound(q_to)
-                @assert JuMP.lower_bound(q_to) <= 0
-                JuMP.set_upper_bound(qt_br[t_idx,0], abs(JuMP.lower_bound(q_to)))
-                JuMP.delete_lower_bound(q_to)
+            if typeof(q_to)==VariableRef
+                if JuMP.has_upper_bound(q_to)
+                    @assert JuMP.upper_bound(q_to) >= 0
+                    JuMP.set_upper_bound(qt_br[t_idx,1], JuMP.upper_bound(q_to))
+                    JuMP.delete_upper_bound(q_to)
+                end
+                if JuMP.has_lower_bound(q_to)
+                    @assert JuMP.lower_bound(q_to) <= 0
+                    JuMP.set_upper_bound(qt_br[t_idx,0], abs(JuMP.lower_bound(q_to)))
+                    JuMP.delete_lower_bound(q_to)
+                end
+            else
+                #TODO
             end
         end
     end
